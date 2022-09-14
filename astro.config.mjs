db0d7f9d10f6fd9ee54e02fcddb1ea8109a24a5f
Fran <fran.siner@gmail.com>
2022-09-14 13:42:48 +0200

    Added astro service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import serviceWorker from "astrojs-service-worker";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), serviceWorker()],
   output: "server",
   adapter: vercel(),
 });
